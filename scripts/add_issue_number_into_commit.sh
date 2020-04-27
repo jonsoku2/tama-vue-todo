14a33c6c06ee69669bac3ba7391f3d8fefda29dc
jonsoku2 <whdtjr2792@gmail.com>
2020-04-28 01:52:33 +0900

    [test] husky - 20

--- scripts/add_issue_number_into_commit.sh
+++ scripts/add_issue_number_into_commit.sh
@@ -1,11 +1,21 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-COMMIT_MSG_FILE=$1
+# This hook works for branches named such as "123-description" and will add "[#123]" to the commit message.
 
-branch_name=`git rev-parse --abbrev-ref HEAD`
-issue_number=`echo ${branch_name} | cut -d '-' -f1`
-first_line=`head -n1 ${COMMIT_MSG_FILE}`
+# get current branch
+branchName=`git rev-parse --abbrev-ref HEAD`
 
-if [ -z "$first_line" ]; then
-    sed -i ".bak" "1s/^/[#$issue_number] /" ${COMMIT_MSG_FILE}
+# search issue id in the branch name, such a "123-description" or "XXX-123-description"
+issueId=$(echo $branchName | sed -nE 's,([A-Z]?-?[0-9]+)-.+,\1,p')
+
+# only prepare commit message if pattern matched and issue id was found
+if [[ ! -z $issueId ]]; then
+ # $1 is the name of the file containing the commit message
+ # We prepend issue number in the beginning (so we can easily see it in history)
+ # And also we append the full issue id (with organization/repository#) prefix,
+ # so this commit hook works for other repositories (frontend) too.
+ echo -e "[#$issueId] ""$(cat $1)""\n[organization/repository#$issueId]" > "$1"
+ # sed -i.bak -e "1s/^/\n\n[$issueId]\n/" $1
+ # echo -e "[$issueId]\n""$(cat $1)" > "$1"
+ # sed -i.bak -e "1s/^/$TRIMMED /" $1
 fi
